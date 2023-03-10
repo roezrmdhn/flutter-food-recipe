5c8fe7f3b3d50cfd59d9684f058329881c825e70
mutiapn <mutiaraputri599@gmail.com>
2023-03-11 03:37:56 +0700

    first commit

---
+++ lib/utils/data.dart
@@ -0,0 +1,315 @@
+List categories = [
+  {"name": "Semua"},
+  {"name": "Terbaru"},
+  {"name": "filter"},
+];
+
+List recipes = [
+  {
+    "id": 102,
+    "name": "Opor Ayam",
+    "image":
+        "assets/images/oporayam.jpeg",
+    "price": "Rp35000",
+    "type": "Makanan Utama",
+    "rate": "4.8",
+    "creator": creators[0],
+    "location": "Jawa Tengah",
+    "is_favorited": false,
+    "description":
+        "Makanan khas yang sering ada pada Hari Raya umat Islam, daging ayam kampung masak dengan..",
+  },
+  {
+    "id": 100,
+    "name": "Martabak Manis",
+    "image":
+    "assets/images/martabakmanis.jpg",
+    "price": "Rp20000",
+    "type": "Makanan Penutup",
+    "rate": "4.9",
+    "creator": creators[0],
+    "location": "Surakarta",
+    "is_favorited": false,
+    "description":
+    "Makanan Penutup dengan berbagai isian topping, khas Bandung yang sudah menjadi makanan favorit di..",
+  },
+  {
+    "id": 104,
+    "name": "Nasi Goreng",
+    "image":
+    "assets/images/nasigoreng.jpg",
+    "price": "Rp25000",
+    "type": "Makanan Utama",
+    "rate": "4.7",
+    "creator": creators[1],
+    "location": "Surabaya",
+    "is_favorited": true,
+    "description":
+    "Makanan Utama yang memakai nasi pera/ sisa kemarin, sangat cocok untuk sarapan ditambah dengan berbagai..",
+  },
+  {
+    "id": 102,
+    "name": "Nila Asam Manis",
+    "image":
+    "assets/images/nila.jfif",
+    "price": "Rp30000",
+    "type": categories[2]["name"],
+    "rate": "4.4",
+    "creator": creators[2],
+    "location": "Bandung",
+    "is_favorited": false,
+    "description":
+    "Makanan Utama biasanya menggunakan Nila Merah, dikarenakan tekstur cocok dengan cara memasaknya yang menggunakan..",
+  },
+  {
+    "id": 103,
+    "name": "Rendang",
+    "image":
+    "assets/images/rendang.jpg",
+    "price": "Rp25000",
+    "type": "Makanan Utama",
+    "rate": "4.7",
+    "creator": creators[0],
+    "location": "Sunda",
+    "is_favorited": false,
+    "description":
+    "Makanan Utama Khas Padang, berasal dari daging dengan siraman santan dan rempah-rempah yang beraneka..",
+  },
+  {
+    "id": 104,
+    "name": "Pisang Coklat",
+    "image":
+    "assets/images/pisangcoklat.jpg",
+    "price": "Rp10000",
+    "type": "Camilan",
+    "rate": "4.3",
+    "creator": creators[1],
+    "location": "Bandung",
+    "is_favorited": false,
+    "description":
+    "Camilan dari pisang yang dibalut dengan kulit tepung dengan isian coklat lumer dengan..",
+  },
+  {
+    "id": 105,
+    "name": "Soto Betawi",
+    "image":
+    "assets/images/sotobetawi.jpg",
+    "price": "Rp15000",
+    "type": "Makanan Pagi",
+    "rate": "4.5",
+    "creator": creators[2],
+    "location": "Jakarta",
+    "is_favorited": false,
+    "description":
+    "Makanan Utama Khas Betawi, sangat cocok untuk Sarapan. Berbeda dengan Soto dari daerah lain dikarenakan Soto Betawi..",
+  },
+  {
+    "id": 104,
+    "name": "Es Cendol",
+    "image":
+    "assets/images/escendol.jpg",
+    "price": "Rp7000",
+    "type": "Minuman",
+    "rate": "4.3",
+    "creator": creators[1],
+    "location": "Jakarta",
+    "is_favorited": false,
+    "description":
+    "Minuman yang sangat segar dengan isian cendol, santan dan gula merah sangat cocok dikala panas terik..",
+  },
+  {
+    "id": 109,
+    "name": "Sate Kambing",
+    "image":
+    "assets/images/satekambing.jpg",
+    "price": "Rp35000",
+    "type": "Makanan Utama",
+    "rate": "4.8",
+    "creator": creators[0],
+    "location": "Surakarta",
+    "is_favorited": true,
+    "description":
+    "Makanan Utama yang menggunakan daging Kambing, sangat populer di Kota Solo dengan Ciri Khas yang..",
+  },
+  {
+    "id": 103,
+    "name": "Timlo Solo",
+    "image":
+    "assets/images/timlo.jpg",
+    "price": "Rp15000",
+    "type": "Makanan Pagi",
+    "rate": "4.7",
+    "creator": creators[2],
+    "location": "Solo",
+    "is_favorited": false,
+    "description":
+    "Makanan Utama Khas Solo yang sangat Cocok untuk Sarapan, berisi bihun, sayuran dan..",
+  },
+  {
+    "id": 105,
+    "name": "Tempe Mendoan",
+    "image":
+        "assets/images/tempe.jpg",
+    "price": "Rp5000",
+    "type": categories[1]["name"],
+    "rate": "4.5",
+    "creator": creators[2],
+    "location": "Surakarta",
+    "is_favorited": false,
+    "description":
+        "Makanan Pendamping, sangat nikmat walaupun hanya sekedar tempe yang dibalur tepung. Namun, dengan bumbu dan racikan..",
+  },
+];
+
+List populars = [
+  {
+    "id": 100,
+    "name": "Martabak Manis",
+    "image":
+        "assets/images/martabakmanis.jpg",
+    "price": "Rp20000",
+    "type": "Makanan Penutup",
+    "rate": "4.9",
+    "creator": creators[0],
+    "location": "Surakarta",
+    "is_favorited": false,
+    "description":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+  },
+  {
+    "id": 104,
+    "name": "Nasi Goreng",
+    "image":
+        "assets/images/nasigoreng.jpg",
+    "price": "Rp25000",
+    "type": "Makanan Utama",
+    "rate": "4.7",
+    "creator": creators[1],
+    "location": "Surabaya",
+    "is_favorited": true,
+    "description":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+  },
+  {
+    "id": 102,
+    "name": "Nila Asam Manis",
+    "image":
+        "assets/images/nila.jfif",
+    "price": "Rp30000",
+    "type": "Makanan Utama",
+    "rate": "4.4",
+    "creator": creators[2],
+    "location": "Bandung",
+    "is_favorited": false,
+    "description":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+  },
+  {
+    "id": 103,
+    "name": "Rendang",
+    "image":
+        "assets/images/rendang.jpg",
+    "price": "Rp25000",
+    "type": "Makanan Utama",
+    "rate": "4.7",
+    "creator": creators[0],
+    "location": "Sunda",
+    "is_favorited": false,
+    "description":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+  },
+  {
+    "id": 104,
+    "name": "Pisang Coklat",
+    "image":
+        "assets/images/pisangcoklat.jpg",
+    "price": "Rp10000",
+    "type": "Camilan",
+    "rate": "4.3",
+    "creator": creators[1],
+    "location": "Bandung",
+    "is_favorited": false,
+    "description":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+  },
+  {
+    "id": 105,
+    "name": "Soto Betawi",
+    "image":
+        "assets/images/sotobetawi.jpg",
+    "price": "Rp15000",
+    "type": "Makanan Pagi",
+    "rate": "4.5",
+    "creator": creators[2],
+    "location": "Jakarta",
+    "is_favorited": false,
+    "description":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+  },
+];
+
+List recommends = [
+  {
+    "id": 104,
+    "name": "Es Cendol",
+    "image":
+        "assets/images/escendol.jpg",
+    "price": "Rp7000",
+    "type": "Minuman",
+    "rate": "4.3",
+    "creator": creators[1],
+    "location": "Jakarta",
+    "is_favorited": false,
+    "description":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+  },
+  {
+    "id": 109,
+    "name": "Sate Kambing",
+    "image":
+        "assets/images/satekambing.jpg",
+    "price": "Rp35000",
+    "type": "Makanan Utama",
+    "rate": "4.8",
+    "creator": creators[0],
+    "location": "Surakarta",
+    "is_favorited": true,
+    "description":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+  },
+  {
+    "id": 103,
+    "name": "Timlo Solo",
+    "image":
+        "assets/images/timlo.jpg",
+    "price": "Rp15000",
+    "type": "Makanan Pagi",
+    "rate": "4.7",
+    "creator": creators[2],
+    "location": "Solo",
+    "is_favorited": false,
+    "description":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+  },
+];
+
+List creators = [
+  {
+    "name": "Rais Ramadhani",
+    "type": "Head Chef",
+    "image":
+        "assets/icons/rais.jpg",
+  },
+  {
+    "name": "Farida Surya",
+    "type": "Food Vlogger",
+    "image":
+        "assets/icons/farida.jfif",
+  },
+  {
+    "name": "Selfia Yustika",
+    "type": "Food Stylist",
+    "image":
+        "assets/icons/sel.jfif",
+  },
+];
